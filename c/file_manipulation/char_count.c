f153b92d530ceab0c336f46e5e4977ec11a5ffae
Jorj Xenos <jorjxenos@gmail.com>
2020-01-30 12:40:08 -0500

    fix program printing out every interation

--- c/file_manipulation/char_count.c
+++ c/file_manipulation/char_count.c
@@ -5,7 +5,7 @@
 main(){
     double nc;
   
-    for (nc = 0; getchar() != EOF; nc++){
-        printf("%.0f\n", nc);
-    }
+    for (nc = 0; getchar() != EOF; nc++)
+        ;
+    printf("%.0f\n", nc);
 }
