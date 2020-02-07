ea49e43464d7ce4c62f80ce61b93ffd7c41d133e
Jorj Xenos <jorjxenos@gmail.com>
2020-02-07 15:35:33 -0500

    quick program to count spaces tabs and new lines

---
+++ c/file_manipulation/count_white_space.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// count blanks tabs and new lines
+
+main(){
+    double nc;
+    int n;
+  while ((c = getchar()) != EOF)
+    for (nc = 0; (c = getchar()) != EOF; nc++)
+        if (isWhiteSpace(c))
+            n++;
+    
+    printf("%d\n", n);
+}
+
+int isWhiteSpace(char c){
+    int wsBool = 0;
+    wsBool = c == '\n' | c == '\t' | c == ' ';
+    return wsBool
+}
+
